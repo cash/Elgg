578bc47d5a2cb1954709fcbb5e58c80e13c83a49
Cash Costello <cash.costello@gmail.com>
2010-08-04 23:15:20 -0400

    added Elgg Install to the page titles

--- views/failsafe/page_shells/install.php
+++ views/failsafe/page_shells/install.php
@@ -24,7 +24,7 @@ header('Expires: Fri, 05 Feb 1982 00:00:00 -0500', TRUE);
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 	<head>
-		<title><?php echo $vars['title']; ?></title>
+		<title>Elgg Install : <?php echo $vars['title']; ?></title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<link rel="SHORTCUT ICON" href="<?php echo $vars['url']; ?>_graphics/favicon.ico" />
 		<link rel="stylesheet" href="<?php echo $vars['url']; ?>install/install.css" type="text/css" />
