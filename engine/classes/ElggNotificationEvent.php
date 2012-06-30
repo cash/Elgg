f5e6a256411afe547eb1d36bb22124af1f2ee21c
Cash Costello <cash.costello@gmail.com>
2012-06-30 10:34:30 -0400

    added class for holding a notification event

---
+++ engine/classes/ElggNotificationEvent.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Notification event
+ */
+class ElggNotificationEvent {
+
+	/* @var string The name of the action/event */
+	protected $event;
+
+	/* @var string The type of the object (entity, relationship, annotation) */
+	protected $object_type;
+
+	/* @var int The identifier of the object (GUID for entity, id for relationship or annotation) */
+	protected $object_id;
+
+	/* @var int The GUID of the user who triggered the event */
+	protected $actor_guid;
+
+
+	/**
+	 * Create a notification event
+	 *
+	 * @param ElggData $object The object of the event (ElggEntity, ElggAnnotation, ElggRelationship)
+	 * @param ElggUser $actor  The user that caused the event (default: logged in user)
+	 * @param string   $event  The name of the event (default: create)
+	 */
+	public function __construct($object, $actor = null, $event = 'create') {
+		if ($object instanceof stdClass) {
+			// creating from stdClass object - useful if serializing to external storage
+			foreach ($object as $key => $value) {
+				$this->$key = $value;
+			}
+		} else {
+			if (!($object instanceof ElggData)) {
+				// @todo find the best message - probably create generic message
+				throw new InvalidParameterException();
+			}
+
+			if (elgg_instanceof($object)) {
+				$this->object_type = 'entity';
+				$this->object_id = $object->getGUID();
+			} else {
+				$this->object_type = $object->getType();
+				$this->object_id = $object->id;
+			}
+
+			if ($actor == null) {
+				$this->actor_guid = elgg_get_logged_in_user_guid();
+			} else {
+				$this->actor_guid = $actor->getGUID();
+			}
+
+			$this->event = $event;
+		}
+	}
+
+	/**
+	 * Get the actor of the event
+	 *
+	 * @return ElggUser
+	 */
+	public function getActor() {
+		return get_entity($this->actor_guid);
+	}
+
+	/**
+	 * Get the object of the event
+	 *
+	 * @return ElggData
+	 */
+	public function getObject() {
+		switch ($this->object_type) {
+			case 'entity':
+				return get_entity($this->object_id);
+				break;
+			case 'relationship':
+				return get_relationship($this->object_id);
+				break;
+			case 'extender':
+				return elgg_get_annotation_from_id($this->object_id);
+				break;
+		}
+		return null;
+	}
+
+	/**
+	 * Get the name of the event
+	 *
+	 * @return string
+	 */
+	public function getName() {
+		return $this->event;
+	}
+}
